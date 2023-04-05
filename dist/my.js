466e65140726158829c8316ae43483456c736e41
tacnewapp <129626601+tacnewapp@users.noreply.github.com>
2023-04-06 03:34:20 +0530

    Add files via upload

---
+++ dist/my.js
@@ -0,0 +1,53 @@
+$(window).load(function(){ // This runs when the window has loaded
+
+var img2 = $("").attr("src", "dist/images/iphone-feature-02.png").load(function() {
+$("#a2").append(img2);
+});
+    var img3 = $("").attr("src", "dist/images/iphone-feature-03.png").load(function() {
+$("#a3").append(img3);
+});
+    
+    var img4 = $("").attr("src", "dist/images/iphone-feature-04.png").load(function() {
+$("#a4").append(img4);
+})
+    var img5 = $("").attr("src", "dist/images/iphone-feature-05.png").load(function() {
+$("#a5").append(img5);
+});
+    var img6 = $("").attr("src", "dist/images/iphone-feature-06.png").load(function() {
+$("#a6").append(img6);
+});
+    
+    var img7 = $("").attr("src", "dist/images/iphone-feature-07.png").load(function() {
+$("#a7").append(img7);
+});
+    var img8 = $("").attr("src", "dist/images/iphone-feature-08.png").load(function() {
+$("#a8").append(img8);
+});
+    var img9 = $("").attr("src", "dist/images/iphone-feature-09.png").load(function() {
+$("#a9").append(img9);
+});
+    var img10 = $("").attr("src", "dist/images/iphone-feature-10.png").load(function() {
+$("#a10").append(img10);
+});
+    var img11 = $("").attr("src", "dist/images/iphone-feature-11.png").load(function() {
+$("#a11").append(img11);
+});
+    var img12 = $("").attr("src", "dist/images/iphone-feature-12.png").load(function() {
+$("#a12").append(img12);
+});
+    var img13 = $("").attr("src", "dist/images/iphone-feature-13.png").load(function() {
+$("#a13").append(img13);
+});
+    var img14 = $("").attr("src", "dist/images/iphone-feature-15.png").load(function() {
+$("#a14").append(img14);
+});
+    
+    var img15 = $("").attr("src", "dist/images/iphone-feature-14.png").load(function() {
+$("#a15").append(img15);
+});
+    
+    var img16 = $("").attr("src", "dist/images/iphone-feature-16.png").load(function() {
+$("#a16").append(img16);
+});
+    
+});
